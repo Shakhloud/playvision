9d71cf46360ebcb3a097ba5060ae6a489bb5e836
Shakhloud <m.shakh-work@mail.ru>
2023-09-13 12:50:30 +0300

    init vue-project

---
+++ plavision-testex/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {}
+</script>
+
+<template>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Karla:wght@300;400&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Karla', sans-serif;
+}
+
+</style>
